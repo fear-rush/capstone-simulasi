d0d8e07b765d9b415b34020117db1b6596e50de4
muhammadfiras <muhfiras1@gmail.com>
2022-05-08 18:04:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import LineChart from "./components/LineChart";
+import BarChart from "./components/BarChart";
+import { SensorData } from "./data/Data";
 
 function App() {
+  const [sensorDataA, setSensorDataA] = useState({
+    labels: SensorData.map((data) => data.time),
+    datasets: [
+      {
+        label: "Temperatur Tanaman",
+        data: SensorData.map((data) => data.temperature),
+        borderColor: "black",
+        borderWidth: 1,
+        fillStyle: "lightGreen",
+      },
+    ],
+  });
+
+  const [sensorDataB, setSensorDataB] = useState({
+    labels: SensorData.map((data) => data.time),
+    datasets: [
+      {
+        label: "Kelembapan Tanah",
+        data: SensorData.map((data) => data.temperature),
+        borderColor: "black",
+        borderWidth: 1,
+        fillStyle: "lightGreen",
+      },
+    ],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500 w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <div className="">
+          <h1 className="text-4xl font-bold text-stone-800 mb-8">
+            User Dashboard
+          </h1>
+        </div>
+        <div className="w-96 flex justify-center">
+          <LineChart chartData={sensorDataA} />
+          <BarChart chartData={sensorDataB} />
+        </div>
+        <h1 className="my-8">
+          Status Solenoid: <i><b>OFF</b></i>
+        </h1>
+        <div className="flex">
+          <button class="btn btn-success mr-6">Hidupkan Solenoid</button>
+          <button class="btn btn-error">Matikan Solenoid</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/Data.js
@@ -0,0 +1,32 @@
+export const SensorData = [
+  {
+    id: 1,
+    time: "08.00",
+    temperature: 19,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    time: "09.00",
+    temperature: 22,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    time: "10.00",
+    temperature: 25,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    time: "11.00",
+    temperature: 28,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    time: "12.00",
+    temperature: 30,
+    userLost: 234,
+  },
+];
